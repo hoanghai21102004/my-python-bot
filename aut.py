8f9a1c03fa2fb210b7de6d8c6491af94a7ecbebc
hoanghai21102004 <hoangmonz210@gmail.com>
2024-06-09 23:40:46 +0700

    Update aut.py

--- aut.py
+++ aut.py
@@ -1,12 +1,23 @@
+import time
 import requests
 import pandas as pd
-import time
+import joblib
+from datetime import datetime, timedelta
+from telegram import Update
+from telegram.ext import Application, CommandHandler, CallbackContext
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
 
+# Tạo danh sách key
+free_key = "FREEKEY123"
+permanent_keys = ["PERMKEY" + str(i) for i in range(1, 11)]
+
+# Lưu trữ thông tin sử dụng key
+used_keys = {}
+telegram_users = {}
+
+admin_username = "@hoanghai2110"
 
 def fetch_historical_results(limit=100):
     url = "https://m.coinvid.com/api/rocket-api/game/issue-result/page"
@@ -40,7 +51,6 @@ def fetch_historical_results(limit=100):
             return data['data']['records']
     return []
 
-
 def preprocess_data(records):
     df = pd.DataFrame(records)
     df['color'] = df['simpleResultFormatList'].apply(lambda x: x[0].get('color') if x else None)
@@ -57,13 +67,23 @@ def preprocess_data(records):
 
     return df[['issue', 'color', 'previous_color', 'color_change', 'issue_diff']]
 
-
-def train_model(df):
+def train_model(df, model_type='xgboost'):
     X = df[['issue', 'previous_color', 'color_change', 'issue_diff']]
     y = df['color']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = XGBClassifier(n_estimators=200, learning_rate=0.05, max_depth=5, random_state=42)
+    
+    if model_type == 'xgboost':
+        model = XGBClassifier(n_estimators=200, learning_rate=0.05, max_depth=5, random_state=42)
+    elif model_type == 'random_forest':
+        model = RandomForestClassifier(n_estimators=200, max_depth=5, random_state=42)
+    elif model_type == 'gradient_boosting':
+        model = GradientBoostingClassifier(n_estimators=200, learning_rate=0.05, max_depth=5, random_state=42)
+    elif model_type == 'svm':
+        model = SVC(probability=True, random_state=42)
+    else:
+        raise ValueError("Invalid model type. Choose from 'xgboost', 'random_forest', 'gradient_boosting', 'svm'.")
+
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -72,6 +92,7 @@ def train_model(df):
     recall = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
 
+    print(f"Model: {model_type}")
     print(f"Accuracy: {accuracy:.2f}")
     print(f"Precision: {precision:.2f}")
     print(f"Recall: {recall:.2f}")
@@ -79,22 +100,68 @@ def train_model(df):
 
     return model
 
-
 def predict_next_result(model, next_issue, last_color, last_color_change, last_issue_diff):
     prediction = model.predict([[next_issue, last_color, last_color_change, last_issue_diff]])
     return 'XANH' if prediction[0] == 0 else 'ĐỎ'
 
+def load_model():
+    try:
+        model = joblib.load('model.pkl')
+        print("Model loaded successfully.")
+        return model
+    except FileNotFoundError:
+        print("No model found. Training a new one.")
+        return None
+
+def save_model(model):
+    joblib.dump(model, 'model.pkl')
+    print("Model saved successfully.")
+
+def check_key(key, user_id):
+    global used_keys, telegram_users
+
+    # Check if the key is a permanent key
+    if key in permanent_keys:
+        if key not in used_keys:
+            used_keys[key] = user_id
+            return True
+        elif used_keys[key] == user_id:
+            return True
+        else:
+            return False
+
+    # Check if the key is the free key
+    elif key == free_key:
+        if user_id not in telegram_users:
+            telegram_users[user_id] = datetime.now()
+            return True
+        else:
+            time_used = datetime.now() - telegram_users[user_id]
+            if time_used < timedelta(minutes=15):
+                return True
+            else:
+                return False
+
+    return False
 
 async def start(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.from_user.id
+    key = context.args[0] if context.args else None
+
+    if not key or not check_key(key, user_id):
+        await update.message.reply_text("Key không hợp lệ hoặc đã hết hạn.")
+        return
+
     context.user_data['last_issue'] = None
     context.user_data['last_color'] = None
     context.user_data['last_color_change'] = None
     context.user_data['last_issue_diff'] = None
-    context.user_data['model'] = None
+    context.user_data['model'] = load_model()
     context.user_data['predictions_count'] = 0
     context.user_data['correct_predictions'] = 0
     context.user_data['running'] = True
-    context.user_data['result_printed'] = False  # Flag to track if the result is printed
+    context.user_data['result_printed'] = False
+    context.user_data['last_prediction'] = None
 
     chaohoi = """────────────────────────────────────────
 =>TOOL DỰ ĐOÁN PHIÊN XANH ĐỎ COINVID VER 1.0
@@ -105,9 +172,13 @@ async def start(update: Update, context: CallbackContext) -> None:
     time.sleep(2)
     await update.message.reply_text("『Bot đã hoạt động, bắt đầu dự đoán phiên...』")
 
-
     while context.user_data['running']:
         try:
+            if key == free_key and datetime.now() - telegram_users[user_id] > timedelta(minutes=15):
+                await update.message.reply_text("Thời gian sử dụng key miễn phí đã hết.")
+                context.user_data['running'] = False
+                return
+
             historical_results = fetch_historical_results(limit=100)
             if historical_results:
                 df = preprocess_data(historical_results)
@@ -118,28 +189,31 @@ async def start(update: Update, context: CallbackContext) -> None:
                 issue_diff = last_result['issue_diff']
 
                 if context.user_data['last_issue'] is None or issue != context.user_data['last_issue']:
-                    if not context.user_data['result_printed']:
-                        await update.message.reply_text(f"🙀 Phiên trước: Phiên : {issue} | Kết quả : {color}")
-                        context.user_data['result_printed'] = True
-
-                    if context.user_data['model'] is None:
-                        context.user_data['model'] = train_model(df)
-
                     if context.user_data['last_issue'] is not None and issue != context.user_data['last_issue']:
-                        next_issue = context.user_data['last_issue'] + 1
-                        next_prediction = predict_next_result(context.user_data['model'], next_issue,
-                                                              context.user_data['last_color'],
-                                                              context.user_data['last_color_change'],
-                                                              context.user_data['last_issue_diff'])
-                        await update.message.reply_text("🥷 Dự đoán phiên tiếp theo: " + next_prediction)
+                        if context.user_data['last_prediction'] is not None:
+                            result_text = f"Phiên : {issue} | Kết quả : {color}"
+                            if context.user_data['last_prediction'] == color:
+                                result_text += " | WIN ✅"
+                                context.user_data['correct_predictions'] += 1
+                            else:
+                                result_text += " | LOSE ⭕"
+                            await update.message.reply_text(result_text)
 
-                        if next_prediction == color:
-                            context.user_data['correct_predictions'] += 1
+                        next_issue = issue + 1
+                        next_prediction = predict_next_result(
+                            context.user_data['model'], next_issue,
+                            last_result['previous_color'],
+                            color_change,
+                            issue_diff
+                        )
+                        context.user_data['last_prediction'] = next_prediction
+                        prediction_text = f"💡 Dự đoán phiên tiếp theo: {next_prediction}"
+                        await update.message.reply_text(prediction_text)
+
                         context.user_data['predictions_count'] += 1
-
                         if context.user_data['predictions_count'] == 10:
                             accuracy = context.user_data['correct_predictions'] / context.user_data['predictions_count']
-                            await update.message.reply_text(f"Tổng số lệnh thắng và thua sau 10 phiên là: {accuracy:.2f}")
+                            await update.message.reply_text(f"Tỷ lệ thắng sau 10 phiên là: {accuracy * 100:.2f}%")
                             context.user_data['predictions_count'] = 0
                             context.user_data['correct_predictions'] = 0
 
@@ -159,21 +233,29 @@ async def start(update: Update, context: CallbackContext) -> None:
 
         time.sleep(5)
 
-
-
 async def stop(update: Update, context: CallbackContext) -> None:
     context.user_data['running'] = False
     await update.message.reply_text("Bot stopped!")
 
+async def show_keys(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user.username
+    if user == admin_username:
+        keys_text = "Danh sách key đã tạo:\n"
+        keys_text += f"Free key: {free_key}\n"
+        keys_text += "Permanent keys:\n" + "\n".join(permanent_keys)
+        await update.message.reply_text(keys_text)
+    else:
+        await update.message.reply_text("Bạn không có quyền truy cập vào lệnh này.")
 
 def main():
-    application = Application.builder().token("7100769571:AAGe3JMkpL62-2ffKtM7dLenDnx6DXDNZBk").build()
+    application = Application.builder().token("7050851037:AAFOT2fxogbG383ubAIMcsA5Jfjuhk8jZVk").build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("stop", stop))
+    application.add_handler(CommandHandler("key", show_keys))
 
     application.run_polling()
 
-
 if __name__ == "__main__":
     main()
+    
